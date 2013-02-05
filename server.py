74db5e4b9b4efef282743489f8a4a7b3859490e2
Kevin Baribeau <kevin.baribeau@gmail.com>
2013-02-05 12:08:19 -0600

    add flask server

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+@app.route('/api/login', methods=['POST'])
+def post():
+  return "post successful!"
+
+if __name__ == "__main__":
+  app.run('localhost', 3000)
